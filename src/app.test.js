8adea25df7774a9de1e52b81150916d57bd92357
vivek <linkvivek.gre@gmail.com>
2018-07-11 22:19:52 -0500

    created test cases

---
+++ src/app.test.js
@@ -0,0 +1,18 @@
+
+
+import React from 'react'
+import { shallow } from 'enzyme'
+import BooksApp from './App.js'
+// import { configure } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+
+// configure({ adapter: new Adapter() });
+
+describe('bla bla', () => {
+    it('testing BooksApp', () => {
+        expect(1+1).toEqual(2)
+        // const component = shallow(<BooksApp/>)
+        // console.log(component)
+    })
+    
+})
